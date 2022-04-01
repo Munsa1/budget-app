452fefe8c538f3abebabc5b1c1b26a2041420c7b
Munsa1 <munsamibenge65@gmail.com>
2022-04-01 17:43:26 +0200

    Fix linters

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,10 @@
 class Group < ApplicationRecord
-    belongs_to :user
-    has_and_belongs_to_many :expenses, dependent: :destroy
-  
-    validates :name, :icon, presence: true, length: { maximum: 255 }
-  
-    def total_expenses
-      expenses.sum(:amount)
-    end
+  belongs_to :user
+  has_and_belongs_to_many :expenses, dependent: :destroy
+
+  validates :name, :icon, presence: true, length: { maximum: 255 }
+
+  def total_expenses
+    expenses.sum(:amount)
   end
+end
