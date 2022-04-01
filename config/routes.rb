92d1df1a259c932e3d42cc8835b1f7ae584b48c7
Munsa1 <munsamibenge65@gmail.com>
2022-04-01 16:06:25 +0200

    Generate expenses controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'expenses/index'
+  get 'expenses/new'
+  get 'expenses/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
